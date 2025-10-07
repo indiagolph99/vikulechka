e86c7b0f2aba87a0d2b52ed84537f2f90bd0c4f8
indiagolph99 <savva.gorodetsky@gmail.com>
2025-10-07 21:17:47 +0300

    работа с эксельками на месте

--- src/renderer/components/HelloWorld.vue
+++ src/renderer/components/HelloWorld.vue
@@ -1,29 +1,35 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { read, utils }  from 'xlsx';
 
 defineProps({
   msg: String,
 })
 
-const count = ref(0)
+const text = ref('')
+
+async function handleFile(e: Event) {
+  const target = e.target as HTMLInputElement;
+  if (target.files) {
+    const file = target.files[0];
+    const data = await file.arrayBuffer();
+    /* data is an ArrayBuffer */
+    const workbook = read(data);
+
+    const jsonData = utils.sheet_to_json(Object.values(workbook.Sheets)[0])
+    console.log("Excel as JSON:", jsonData[0])
+    text.value = JSON.stringify(jsonData[0])
+
+    /* DO SOMETHING WITH workbook HERE */
+  }
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    {{ text }}
+    <input type="file" name="xlfile" id="xlf" @change="handleFile" />
   </div>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
